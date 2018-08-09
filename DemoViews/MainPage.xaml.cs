ada2c7c632ea373623769daf8fe53b83f945b577
lorenaquint <lorenaquint@gmail.com>
2018-08-09 10:13:47 -0500

    creando webview

--- DemoViews/MainPage.xaml.cs
+++ DemoViews/MainPage.xaml.cs
@@ -12,6 +12,13 @@ namespace DemoViews
         public MainPage()
         {
             InitializeComponent();
+			timPickInicial.Time = new TimeSpan(17, 0, 0);
+        }
+		async void Handle_Navigated(object sender, Xamarin.Forms.WebNavigatedEventArgs e)
+        {
+            await DisplayAlert("Completado",
+                               "Se ha completado la carga de la Página",
+                               "Ok");
         }
     }
 }
